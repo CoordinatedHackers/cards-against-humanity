ae9922f90f0c81fed806ce7bf27eb0b566f2c4db
Sidney San Martín <sidney@s4y.us>
2013-11-09 16:58:23 -0500

    An abstraction for a deck

---
+++ public/js/deck.js
@@ -0,0 +1,16 @@
+function Deck(cards) {
+	this.discards = cards;
+	this.shuffle();
+}
+
+Deck.prototype.shuffle = function() {
+	shuffle((this.deck = this.discards));
+	this.discards = [];
+};
+
+Deck.prototype.draw = function() {
+	if (!this.deck.length) { this.shuffle(); }
+	return this.deck.pop();
+}
+
+Deck.prototype.discard = function(card) { this.discards.push(card); }
