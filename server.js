aeffea755c01c9edb1db251393b980e9ac53097b
Sam Epstein <same008@gmail.com>
2013-11-09 10:36:54 -0500

    Breakfast time, come on grab your friends...

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 // https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('--91jAYB4WnLzw4a');
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
 
 var isProduction = (process.env.NODE_ENV === 'production');
 var http = require('http');
-var port = (isProduction ? 80 : 8000);
+var port = (isProduction ? 80 : 5000);
 
-http.createServer(function (req, res) {
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/coordinated-hackers" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+app.use(express.static(__dirname + '/public'));
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body>' + voteko + '</body></html>\n');
-}).listen(port, function(err) {
+app.get("/", function(req, res) {
+	res.send("Oh Herrow World");
+});
+
+io.sockets.on("connection", function(socket) {
+	socket.on("yo", function(data) {
+		console.log("Yo", data);
+		socket.emit("yo", {oh: "herrow"});
+	});
+});
+
+server.listen(port, function(err) {
   if (err) { console.error(err); process.exit(-1); }
 
   // if run as root, downgrade to the owner of this file
@@ -24,3 +35,4 @@ http.createServer(function (req, res) {
 
   console.log('Server running at http://0.0.0.0:' + port + '/');
 });
+
