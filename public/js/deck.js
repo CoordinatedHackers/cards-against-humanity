3d8d148b60734e1647dcbcd31f6d138c583b177e
Sidney San Martín <sidney@s4y.us>
2013-11-09 21:36:07 -0500

    Forgot to move this, derp

--- public/js/deck.js
+++ public/js/deck.js
@@ -1,3 +1,15 @@
+// Credit: http://stackoverflow.com/a/6274398/84745
+function shuffle(array) {
+	var counter = array.length, temp, index;
+	while (counter--) {
+		index = (Math.random() * counter) | 0;
+		temp = array[counter];
+		array[counter] = array[index];
+		array[index] = temp;
+	}
+	return array;
+}
+
 function Deck(cards) {
 	this.discards = cards;
 	this.shuffle();
